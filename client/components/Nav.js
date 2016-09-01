232cad0bb25b0b49f52d41d4e81d6390dd43bac8
syed nabeel <snabe@allstate.com>
2016-09-01 10:23:17 -0500

    first !

---
+++ client/components/Nav.js
@@ -0,0 +1,33 @@
+/* eslint-disable max-len, jsx-a11y/href-no-hash */
+
+import React from 'react';
+import { Link } from 'react-router';
+
+export default () => (
+  <nav className="navbar navbar-default">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+        </button>
+        <a className="navbar-brand" href="#">Fitness</a>
+      </div>
+
+      <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul className="nav navbar-nav" />
+        <ul className="nav navbar-nav navbar-right">
+          <li><Link to="/"><i className="fa fa-home fa-fw" /> Home</Link></li>
+          <li><Link to="/about"><i className="fa fa-hashtag fa-fw" /> About</Link></li>
+          <li><Link to="/faq"><i className="fa fa-question-circle-o fa-fw" /> Faq</Link></li>
+          <li><Link to="/register"><i className="fa fa-user fa-fw" /> Register</Link></li>
+          <li><Link to="/login"><i className="fa fa-key fa-lock" /> Login</Link></li>
+          <li><Link to="/logout"><i className="fa fa-key fa-unlock" /> Logout</Link></li>
+          <li><Link to="/profile"><i className="fa fa-key fa-heartbeat" /> Profile</Link></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
---
+++ client/index.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/imports-first */
+/* global document */
+
+import App from './components/App';
+import Home from './components/Home';
+import About from './components/About';
+import Faq from './components/Faq';
+import Register from './components/Register';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="about" component={About} />
+      <Route path="faq" component={Faq} />
+      <Route path="register" component={Register} />
+      <Route path="login" component={Login} />
+      <Route path="logout" component={Logout} />
+      <Route path="profile" component={Profile} />
+    </Route>
+  </Router>
+  , document.getElementById('root'));
